12cd335c59b6162da6c94523f11f5f7432aa0981
C5-tachibana <tachibana@c-five.net>
2022-12-03 23:06:00 +0900

    ♻️ Linksリファクタリング

--- components/Headline.js
+++ components/Headline.js
@@ -1,11 +1,9 @@
 import classes from './Headline.module.css'
 
 export function Headline(props) {
-  console.log(props);
   return (
     <div>
       <h1 className={classes.title}>{props.page} page</h1>
-
       <p className={classes.description}>
         Get started by editing{' '}{props.children}
       </p>
