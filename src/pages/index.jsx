c370bc39f6c55f7a201380bb9809592da77f6c9a
C5-tachibana <tachibana@c-five.net>
2022-12-04 15:04:06 +0900

    ⚗️ 不要な記述、クリックイベントの実験

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,18 @@ import styles from 'src/styles/Home.module.css'
 import { Header } from 'src/components/Header'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
+import { useCallback } from 'react'
+
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo)
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +23,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a 
+        href="/about" onClick={handleClick}
+      >ボタン</a>
       <Main page="index"/>
 
       <Footer />
